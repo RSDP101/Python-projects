79043ec1aff6f4a543a9cb216a54c8e187c8e946
RSDP101 <129794940+RSDP101@users.noreply.github.com>
2023-05-22 22:21:21 -0300

    Delete find_all.py

--- find_all.py
+++
@@ -1,7 +0,0 @@
-import re
-x= 'olha eu, eu, eu! 21 12 aqui 21eu tambem'
-a= input("Pesquisar: ")
-y= re.findall(a, x)
-print(y)
-c = (len(y));
-print ("Tem " + str(c) + " ocorrências da sua string" )
