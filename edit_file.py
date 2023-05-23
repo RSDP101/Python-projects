ebcd6fabb3a4511b89c7899309bc76d09f43f5c3
RSDP101 <129794940+RSDP101@users.noreply.github.com>
2023-05-22 22:03:25 -0300

    Add files via upload

---
+++ edit_file.py
@@ -0,0 +1,21 @@
+import string
+
+def strip_punc(line):
+    for character in string.punctuation:
+        #remove characters in string.punctuation
+        line = line.replace (character, "")
+        words=line.split()
+        for word in words:
+            #make all words become lowercase
+            line=line.replace(word,word.lower())        
+    return line
+
+with open ("alice.txt", "r") as alice:
+    #creates a list of lines for the file
+    lines = alice.readlines()
+with open ("alice.txt", "w") as alice:
+    for line in lines:
+        new_line = strip_punc(line)
+        alice.write (new_line)
+
+    
